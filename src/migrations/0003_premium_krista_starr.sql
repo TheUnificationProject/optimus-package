fb1869c201361da8eb24be0a728bc6687fd2bf8f
Clément <clementfiesta@gmail.com>
2026-01-17 18:58:14 +0100

    feat: Add location, contact email, and contact phone number to profiles table and update migration journal

---
+++ src/migrations/0003_premium_krista_starr.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "profiles" ADD COLUMN "location" varchar(128);--> statement-breakpoint
+ALTER TABLE "profiles" ADD COLUMN "contact_email" varchar(320);--> statement-breakpoint
+ALTER TABLE "profiles" ADD COLUMN "contact_phone_number" varchar(32);
