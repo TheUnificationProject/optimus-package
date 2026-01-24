38abda40021e72ae37354c0007d6fe5f00c9070e
Clément <clementfiesta@gmail.com>
2026-01-24 12:09:44 +0100

    feat: Add `lang` field & indexes to `contactMessageSchema`

--- src/schemas/contact-message.schema.ts
+++ src/schemas/contact-message.schema.ts
@@ -1,5 +1,13 @@
 import { profileSchema } from '@schemas/profile.schema';
-import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { userSchema } from '@schemas/user.schema';
+import {
+  index,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 export const FIRST_NAME_MIN_LENGTH = 1;
 export const FIRST_NAME_MAX_LENGTH = 64;
@@ -14,22 +22,38 @@ export const EMAIL_MAX_LENGTH = 320;
 
 export const PHONE_NUMBER_MAX_LENGTH = 32;
 
-export const contactMessageSchema = pgTable('contact_messages', {
-  uuid: uuid('uuid').primaryKey().defaultRandom(),
-  profileUuid: uuid('profile_uuid')
-    .notNull()
-    .references(() => profileSchema.uuid, { onDelete: 'cascade' }),
+export const LANG_MAX_LENGTH = 5;
 
-  firstName: varchar('first_name', { length: FIRST_NAME_MAX_LENGTH }).notNull(),
-  lastName: varchar('last_name', { length: LAST_NAME_MAX_LENGTH }).notNull(),
-  organizationName: varchar('organization_name', {
-    length: ORGANIZATION_NAME_MAX_LENGTH,
-  }),
-  email: varchar('email', { length: EMAIL_MAX_LENGTH }).notNull(),
-  phoneNumber: varchar('phone_number', { length: PHONE_NUMBER_MAX_LENGTH }),
-  message: text('message').notNull(),
+export const contactMessageSchema = pgTable(
+  'contact_messages',
+  {
+    uuid: uuid('uuid').primaryKey().defaultRandom(),
+    profileUuid: uuid('profile_uuid')
+      .notNull()
+      .references(() => profileSchema.uuid, { onDelete: 'cascade' }),
 
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-});
+    firstName: varchar('first_name', {
+      length: FIRST_NAME_MAX_LENGTH,
+    }).notNull(),
+    lastName: varchar('last_name', { length: LAST_NAME_MAX_LENGTH }).notNull(),
+    organizationName: varchar('organization_name', {
+      length: ORGANIZATION_NAME_MAX_LENGTH,
+    }),
+    email: varchar('email', { length: EMAIL_MAX_LENGTH })
+      .notNull()
+      .references(() => userSchema.email),
+    phoneNumber: varchar('phone_number', { length: PHONE_NUMBER_MAX_LENGTH }),
+    message: text('message').notNull(),
+
+    // METADATA
+    lang: varchar('lang', { length: LANG_MAX_LENGTH }),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+  },
+  (table) => [
+    index('email_idx').on(table.email),
+    index('phone_number_idx').on(table.phoneNumber),
+    index('created_at_idx').on(table.createdAt),
+  ],
+);
